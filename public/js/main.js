5964a3d08d2c2b5cb935bf227f673dc78cea21c1
jakerutl <j_rutledge3@fanshaweonline.ca>
2018-03-23 11:26:46 -0400

    front and back end both working

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+const myApp = {
+  //some non-BM functionality would go here
+
+
+   vm : new Vue({
+    el : "#app",
+
+    data : {
+      message : "welcome to my Netflix ripoff!"
+    },
+
+    methods : {
+
+    },
+
+    delimiters : ["${", "}"]
+
+  })
+}
