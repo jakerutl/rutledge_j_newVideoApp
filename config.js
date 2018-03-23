81d54941b28af6b4300352748f38bcee317fdc96
jakerutl <j_rutledge3@fanshaweonline.ca>
2018-03-23 11:59:39 -0400

    completed bringing in the data

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+	user: "root",
+	password: "root",
+	port: 8889,
+	host: "localhost",
+	database: "movies",
+}
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var config = require('../config');
+var connect = require('../utils/sqlConnect');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { message : 'All done!' });
+
+  console.log('get all the movies');
+  connect.query(`SELECT * FROM tbl_movies m, tbl_genre g, tbl_mov_genre mg WHERE m.movies_id = mg.movies_id AND g.genre_id = mg.genre_id`,(error, rows)=>{
+    if(error){
+      console.log(error);
+    }else{
+      res.render('home', { defaultMovie : rows[Math.floor(Math.random() * rows.length)],
+        data : JSON.stringify(rows)
+      });
+    }
+  });
+
+  // res.render('home', { message : 'All done!' });
 });
 
 module.exports = router;
